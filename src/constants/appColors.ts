6b53f91ff912018db84f4e56cd2d21479095d963
Doanhaiduy <haiduytbt2k3@gmail.com>
2024-04-30 22:49:05 +0700

    feat/onboarding

--- src/constants/appColors.ts
+++ src/constants/appColors.ts
@@ -2,6 +2,6 @@ export const appColors = {
     primary: '#5669FF',
     white: '#FFFFFF',
     text: '#120D26',
-    gray: '807A7A',
+    gray: '#807A7A',
     gray2: '#DADADA',
 };
--- src/navigators/AuthNavigator.tsx
+++ src/navigators/AuthNavigator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LoginScreen } from '../screens';
+import { LoginScreen, OnboardingScreen } from '../screens';
 
 export default function AuthNavigator() {
     const Stack = createNativeStackNavigator();
@@ -10,6 +10,7 @@ export default function AuthNavigator() {
                 headerShown: false,
             }}
         >
+            <Stack.Screen name='OnboardingScreen' component={OnboardingScreen} />
             <Stack.Screen name='LoginScreen' component={LoginScreen} />
         </Stack.Navigator>
     );
